5df245b033ac3506aefe385b5b3dea015e1f332a
Arish Mudra Rakshasa-Loots <73881703+arishmr@users.noreply.github.com>
2026-02-15 13:52:05 +0000

    Add files via upload

---
+++ 03-demos.R
@@ -0,0 +1,209 @@
+## Reduce data to relevant columns for demographics
+demodata <- alldata %>% dplyr::select(-c(pid, phq1, phq2, phq3, phq4, phq5, phq6, phq7, phq8, phq9))
+
+##########################
+## Calculate median and IQR for continuous variables
+##########################
+
+## Create a longer-form dataframe with continuous variables in one column (age, bmi, PHQ, immigrate_duration)
+contdemos <- demodata %>%
+  tidyr::gather(key = "variable",
+                value = "value",
+                age, bmi, PHQ, immigrate_duration) %>%
+  tidyr::drop_na(value)
+
+## Split dataset by demographic variables
+group_names <- contdemos %>% group_keys(variable) %>% pull(1)
+splitdata <- contdemos %>%
+  group_by(variable) %>%
+  group_split() %>%
+  rlang::set_names(group_names)
+rm(group_names)
+
+## Create a blank dataframe in which the outputs will be populated
+
+output <- data.frame(
+  variable = character(0),
+  group = character(0),
+  N = numeric(0),
+  Median = numeric(0),
+  Q25 = numeric(0),
+  Q75 = numeric(0),
+  stringsAsFactors = TRUE
+)
+anova <- data.frame(
+  variable = character(0),
+  predictor = character(0),
+  Df = numeric(0),
+  Fstat = numeric(0),
+  p.value = numeric(0),
+  stringsAsFactors = TRUE
+)
+
+## Now apply the summarise function over each element of the splitdata
+output <- lapply(splitdata, function (x) {
+  # grouped by ethnicity
+  y <-  x %>%
+    group_by(group = ethnicity) %>% 
+    summarise(
+      N = sum(!is.na(value)),  
+      Median = median(value, na.rm = T), 
+      Q25 = quantile(value, 0.25, na.rm = T),
+      Q75 = quantile(value, 0.75, na.rm = T)
+    )
+  y <- y %>% mutate(variable = rep(unique(x$variable))) %>% relocate(variable)
+  y <- dplyr::bind_rows(output, y)
+  y
+  # grouped by immigration status
+  z <-  x %>%
+    group_by(group = immigration) %>% 
+    summarise(
+      N = sum(!is.na(value)),  
+      Median = median(value, na.rm = T), 
+      Q25 = quantile(value, 0.25, na.rm = T),
+      Q75 = quantile(value, 0.75, na.rm = T)
+    )
+  z <- z %>% mutate(variable = rep(unique(x$variable))) %>% relocate(variable)
+  z <- dplyr::bind_rows(y, z)
+  z
+  # overall
+  a <-  x %>%
+    summarise(
+      N = sum(!is.na(value)),  
+      Median = median(value, na.rm = T), 
+      Q25 = quantile(value, 0.25, na.rm = T),
+      Q75 = quantile(value, 0.75, na.rm = T)
+    )
+  a <- a %>% mutate(variable = rep(unique(x$variable))) %>% relocate(variable)
+  a <- a %>% mutate(group = rep("Overall")) %>% relocate(.after = variable)
+  a <- dplyr::bind_rows(z, a)
+  a
+})
+output <- purrr::reduce(output, full_join)
+
+splitdata$immigrate_duration <- NULL
+
+## Now apply the anova function over each element of the splitdata
+anova <- lapply(splitdata, function (x) {
+  y <- summary(aov(value ~ ethnicity, data = x))
+  Df <- y[[1]][1,1]
+  Fstat <- y[[1]][1,4]
+  p.value <- y[[1]][1,5]
+  y <- data.frame(variable = unique(x$variable), predictor = "ethnicity", Df, Fstat, p.value)
+  y <- dplyr::bind_rows(anova, y)
+  y
+  z <- summary(aov(value ~ immigration, data = x))
+  Df <- z[[1]][1,1]
+  Fstat <- z[[1]][1,4]
+  p.value <- z[[1]][1,5]
+  z <- data.frame(variable = unique(x$variable), predictor = "immigration", Df, Fstat, p.value)
+  z <- dplyr::bind_rows(z, y)
+  z
+})
+anova <- purrr::reduce(anova, full_join)
+
+write.csv(output, "results/contdemos.csv", row.names = FALSE)
+write.csv(anova, "results/contanova.csv", row.names = FALSE)
+rm(contdemos, output, splitdata, anova)
+
+
+
+
+
+##########################
+## Calculate proportions for categorical variables
+##########################
+
+## Create a longer-form dataframe with categorical variables in one column (sex, income, alcohol, smoking, insomnia, fatherpsych, motherpsych, immigrate_stage)
+catdemos <- demodata %>%
+  tidyr::gather(key = "variable",
+                value = "value",
+                sex, income, alcohol, smoking, insomnia, fatherpsych, motherpsych, immigrate_stage) %>%
+  tidyr::drop_na(value)
+
+## Split dataset by demographic variables
+group_names <- catdemos %>% group_keys(variable) %>% pull(1)
+splitdata <- catdemos %>%
+  group_by(variable) %>%
+  group_split() %>%
+  rlang::set_names(group_names)
+rm(group_names)
+
+## ensure that values are classed as factors
+splitdata <- lapply(splitdata, function (x) {
+  x$value <- as.factor(x$value)
+  x
+})
+
+## Create a blank dataframe in which the outputs will be populated
+
+output <- data.frame(
+  variable = character(0),
+  group = character(0),
+  value = character(0),
+  N = numeric(0),
+  Freq = numeric(0),
+  stringsAsFactors = TRUE
+)
+chisq <- data.frame(
+  variable = character(0),
+  predictor = character(0),
+  Df = numeric(0),
+  chi = numeric(0),
+  p.value = numeric(0),
+  stringsAsFactors = TRUE
+)
+
+## Now apply the summarise function over each element of the splitdata
+
+output <- lapply(splitdata, function (x) {
+  # grouped by ethnicity
+  y <- x %>% group_by(group = ethnicity, value) %>%
+    summarise(N = n()) %>%
+    mutate(Freq = N / sum(N)*100) %>%
+    mutate(variable = rep(unique(x$variable))) %>% relocate(variable)
+  y <- dplyr::bind_rows(output, y)
+  y
+  # grouped by immigration status
+  z <- x %>% group_by(group = immigration, value) %>%
+    summarise(N = n()) %>%
+    mutate(Freq = N / sum(N)*100) %>%
+    mutate(variable = rep(unique(x$variable))) %>% relocate(variable)
+  z <- dplyr::bind_rows(y, z)
+  z
+  # overall
+  a <-  x %>% group_by(value) %>%
+    summarise(N = n()) %>%
+    mutate(Freq = N / sum(N)*100) %>%
+    mutate(variable = rep(unique(x$variable))) %>% relocate(variable)
+  a <- a %>% mutate(variable = rep(unique(x$variable))) %>% relocate(variable)
+  a <- a %>% mutate(group = rep("Overall")) %>% relocate(.after = variable)
+  a <- dplyr::bind_rows(z, a)
+  a
+})
+output <- purrr::reduce(output, full_join)
+
+splitdata$immigrate_stage <- NULL
+
+## Now apply the chisq function over each element of the splitdata
+chisq <- lapply(splitdata, function (x) {
+  y <- chisq.test(x$value, x$ethnicity)
+  Df <- as.numeric(y[[2]])
+  chi <- as.numeric(y[[1]])
+  p.value <- y[[3]]
+  y <- data.frame(variable = unique(x$variable), predictor = "ethnicity", Df, chi, p.value)
+  y <- dplyr::bind_rows(chisq, y)
+  y
+  z <- chisq.test(x$value, x$immigration)
+  Df <- as.numeric(z[[2]])
+  chi <- as.numeric(z[[1]])
+  p.value <- z[[3]]
+  z <- data.frame(variable = unique(x$variable), predictor = "immigration", Df, chi, p.value)
+  z <- dplyr::bind_rows(y, z)
+  z
+})
+chisq <- purrr::reduce(chisq, full_join)
+
+write.csv(output, "results/catdemos.csv", row.names = F)
+write.csv(chisq, "results/catchisq.csv", row.names = FALSE)
+rm(catdemos, output, splitdata, chisq)
---
+++ 06-logreg-diab.R
@@ -0,0 +1,219 @@
+## GOAL: within people who self-reported lifetime diagnoses of depression AND within each immigration and ethnicity sub-group, calculate Odds Ratios for self-reported diabetes
+
+library(dplyr)
+library(tidyr)
+
+data <- alldata %>% dplyr::filter(depression == TRUE)
+
+##############################
+## COMPARING ETHNICITY GROUPS
+##############################
+
+## create blank dataframe to populate with outputs of logistic regression models
+output <- data.frame(
+  ethnicity = character(0),
+  immigration = character(0),
+  comparison = character(0),
+  outcome = character(0),
+  model = character(0),
+  n = numeric(0),
+  OR = numeric(0),
+  ci.min = numeric(0),
+  ci.max = numeric(0),
+  p.value = numeric(0),
+  stringsAsFactors = F
+)
+
+## Create a generic function:
+# (1) subset the dataset to relevant groups and outcome variables,
+# (2) run 3x logistic regression models, and
+# (3) add the results from the models to the output dataframe
+
+logreg_ethnicity <- function (data, subset, outVar) {
+  
+  if (subset == "Overall") df <- data
+  else
+    #subset data to desired immigration status level
+    df <- data %>% dplyr::filter(immigration == subset)
+  
+  # Minimally adjusted model
+  fit1 <- glm(as.formula(paste(outVar, "~ ethnicity + age + sex")),
+              family = binomial(link="logit"),
+              data = df)
+  ## extract relevant estimates from the model
+  estimates <- data.frame(
+    ethnicity = fit1$xlevels$ethnicity[-1],
+    immigration = rep(subset),
+    comparison = rep("vs White"),
+    outcome = rep(outVar),
+    model = "minimal",
+    n = rep(nobs(fit1)),
+    OR = as.numeric(exp(coef(summary(fit1))[2:6])),
+    ci.min = as.numeric(exp(confint.default(fit1))[2:6,1]),
+    ci.max = as.numeric(exp(confint.default(fit1))[2:6,2]),
+    p.value = as.numeric(coef(summary(fit1))[2:6,4]))
+  ## add these to the main outputs dataframe
+  output <- bind_rows(output, estimates)
+  
+  # BMI-adjusted model
+  fit1 <- glm(as.formula(paste(outVar, "~ ethnicity + age + sex + bmi")),
+              family = binomial(link="logit"),
+              data = df)
+  ## extract relevant estimates from the model
+  estimates <- data.frame(
+    ethnicity = fit1$xlevels$ethnicity[-1],
+    immigration = rep(subset),
+    comparison = rep("vs White"),
+    outcome = rep(outVar),
+    model = "bmi",
+    n = rep(nobs(fit1)),
+    OR = as.numeric(exp(coef(summary(fit1))[2:6])),
+    ci.min = as.numeric(exp(confint.default(fit1))[2:6,1]),
+    ci.max = as.numeric(exp(confint.default(fit1))[2:6,2]),
+    p.value = as.numeric(coef(summary(fit1))[2:6,4]))
+  ## add these to the main outputs dataframe
+  output <- bind_rows(output, estimates)
+  
+  # Fully adjusted model
+  fit1 <- glm(as.formula(paste(outVar, "~ ethnicity + age + sex + bmi + income + alcohol + smoking + social + insomnia + fatherpsych + motherpsych")),
+              family = binomial(link="logit"),
+              data = df)
+  ## extract relevant estimates from the model
+  estimates <- data.frame(
+    ethnicity = fit1$xlevels$ethnicity[-1],
+    immigration = rep(subset),
+    comparison = rep("vs White"),
+    outcome = rep(outVar),
+    model = "full",
+    n = rep(nobs(fit1)),
+    OR = as.numeric(exp(coef(summary(fit1))[2:6])),
+    ci.min = as.numeric(exp(confint.default(fit1))[2:6,1]),
+    ci.max = as.numeric(exp(confint.default(fit1))[2:6,2]),
+    p.value = as.numeric(coef(summary(fit1))[2:6,4]))
+  ## add these to the main outputs dataframe
+  output <- bind_rows(output, estimates)
+  return(output)
+}  
+
+## Now run this function for each subset and outcome
+output <- logreg_ethnicity(data, "Overall", "diabetes")
+output <- logreg_ethnicity(data, "Non-Immigrant", "diabetes")
+output <- logreg_ethnicity(data, "Immigrant", "diabetes")
+
+# correct p values for multiple comparisons
+output$p.fdr <- p.adjust(output$p.value, method = "fdr")
+
+## Export output dataframe
+write.csv(output, "results/logreg_diab_ethnicity.csv", row.names = FALSE)
+rm(logreg_ethnicity, output)
+
+
+
+
+
+
+##############################
+## COMPARING IMMIGRATION STATUS GROUPS
+##############################
+
+## create blank dataframe to populate with outputs of logistic regression models
+output <- data.frame(
+  ethnicity = character(0),
+  immigration = character(0),
+  comparison = character(0),
+  outcome = character(0),
+  model = character(0),
+  n = numeric(0),
+  OR = numeric(0),
+  ci.min = numeric(0),
+  ci.max = numeric(0),
+  p.value = numeric(0),
+  stringsAsFactors = F
+)
+
+## Create a generic function:
+# (1) subset the dataset to relevant groups and outcome variables,
+# (2) run 3x logistic regression models, and
+# (3) add the results from the models to the output dataframe
+
+logreg_immigration <- function (data, subset, outVar) {
+  
+  if (subset == "Overall") df <- data
+  else
+    #subset data to desired immigration status level
+    df <- data %>% dplyr::filter(ethnicity == subset)
+  
+  # Minimally adjusted model
+  fit1 <- glm(as.formula(paste(outVar, "~ immigration + age + sex")),
+              family = binomial(link="logit"),
+              data = df)
+  ## extract relevant estimates from the model
+  estimates <- data.frame(
+    ethnicity = rep(subset),
+    immigration = fit1$xlevels$immigration[-1],
+    comparison = rep("vs Non-Immigrant"),
+    outcome = rep(outVar),
+    model = "minimal",
+    n = rep(nobs(fit1)),
+    OR = as.numeric(exp(coef(summary(fit1))[2])),
+    ci.min = as.numeric(exp(confint.default(fit1))[2,1]),
+    ci.max = as.numeric(exp(confint.default(fit1))[2,2]),
+    p.value = as.numeric(coef(summary(fit1))[2,4]))
+  ## add these to the main outputs dataframe
+  output <- bind_rows(output, estimates)
+  
+  # BMI-adjusted model
+  fit1 <- glm(as.formula(paste(outVar, "~ immigration + age + sex + bmi")),
+              family = binomial(link="logit"),
+              data = df)
+  ## extract relevant estimates from the model
+  estimates <- data.frame(
+    ethnicity = rep(subset),
+    immigration = fit1$xlevels$immigration[-1],
+    comparison = rep("vs Non-Immigrant"),
+    outcome = rep(outVar),
+    model = "bmi",
+    n = rep(nobs(fit1)),
+    OR = as.numeric(exp(coef(summary(fit1))[2])),
+    ci.min = as.numeric(exp(confint.default(fit1))[2,1]),
+    ci.max = as.numeric(exp(confint.default(fit1))[2,2]),
+    p.value = as.numeric(coef(summary(fit1))[2,4]))
+  ## add these to the main outputs dataframe
+  output <- bind_rows(output, estimates)
+  
+  # Fully adjusted model
+  fit1 <- glm(as.formula(paste(outVar, "~ immigration + age + sex + bmi + income + alcohol + smoking + social + insomnia + fatherpsych + motherpsych")),
+              family = binomial(link="logit"),
+              data = df)
+  ## extract relevant estimates from the model
+  estimates <- data.frame(
+    ethnicity = rep(subset),
+    immigration = fit1$xlevels$immigration[-1],
+    comparison = rep("vs Non-Immigrant"),
+    outcome = rep(outVar),
+    model = "full",
+    n = rep(nobs(fit1)),
+    OR = as.numeric(exp(coef(summary(fit1))[2])),
+    ci.min = as.numeric(exp(confint.default(fit1))[2,1]),
+    ci.max = as.numeric(exp(confint.default(fit1))[2,2]),
+    p.value = as.numeric(coef(summary(fit1))[2,4]))
+  ## add these to the main outputs dataframe
+  output <- bind_rows(output, estimates)
+  return(output)
+}  
+
+## Now run this function for each subset and outcome
+output <- logreg_immigration(data, "Overall", "diabetes")
+output <- logreg_immigration(data, "Arab", "diabetes")
+output <- logreg_immigration(data, "Black", "diabetes")
+output <- logreg_immigration(data, "Mixed or Multiple Heritage", "diabetes")
+output <- logreg_immigration(data, "Other Asian", "diabetes")
+output <- logreg_immigration(data, "South Asian", "diabetes")
+output <- logreg_immigration(data, "White", "diabetes")
+
+# correct p values for multiple comparisons
+output$p.fdr <- p.adjust(output$p.value, method = "fdr")
+
+## Export output dataframe
+write.csv(output, "results/logreg_diab_immigration.csv", row.names = FALSE)
+rm(logreg_immigration, output)
